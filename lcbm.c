6940bd887f6c5d6a8d36a5106eb91eade1fcc7ee
Leonardo Castelo Branco de Melo <cbmelo97@gmail.com>
2018-09-08 07:02:41 -0300

    Update lcbm.c

--- lcbm.c
+++ lcbm.c
@@ -9,19 +9,14 @@ typedef struct node{
 int main(void){
     unsigned int cases;
     scanf("%u", &cases);
-
     while(cases--){
+        Node *table[100] = {0};
+        
         unsigned short tableLenght, keyAmmount;
         scanf("%hu %hu", &tableLenght, &keyAmmount);
-        
-        // Initialize
-        Node *table[100] = {0};
-
-        // Insert
-        for (short key = 0; key < keyAmmount; key++){
+        while(keyAmmount--){
             unsigned short value;
             scanf("%hu", &value);
-
             Node *new = malloc(sizeof(Node));
             new -> value = value;
             new -> next = NULL;
@@ -34,12 +29,9 @@ int main(void){
                 while (collision -> next != NULL){
                     collision = collision -> next;
                 };
-
                 collision -> next = new;
             };
         };
-
-        // Print
         for (unsigned short index = 0; index < tableLenght; index++){
             Node *pointer = table[index];
             printf("%hu -> ", index);
@@ -48,13 +40,10 @@ int main(void){
                 printf("%hu -> ", pointer -> value);
                 pointer = pointer -> next;
             };
-
             printf("\\\n");
         };
-
         printf("\n");
         free(table[tableLenght]);
     };
-
     return 0;
 };
